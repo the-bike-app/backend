29fe7879e6fcd8b444245b748dd6465fe6423430
DWilliamZero <william.zero@gigkloud.com>
2020-03-16 16:07:18 -0400

    debug cors issue

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express');
-//const cors = require('cors')
+const cors = require('cors')
 const bodyParser = require('body-parser');
 const logger = require('morgan');
 const routes = require('./routes');
 
 const app = express();
 
-//app.use(cors())
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+app.use(cors())
 app.use(bodyParser.json())
 app.use(logger('dev'))
 
