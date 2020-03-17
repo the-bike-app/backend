44e6153776e9b85a92a4db1bc1d7ba6ab3e48b5e
DWilliamZero <william.zero@gigkloud.com>
2020-03-17 14:13:39 -0400

    fix email bug

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors())
 
 app.all('*', (req, res, next) => {
-  var origin = req.get('origin');
+  const origin = req.get('origin');
   res.header('Access-Control-Allow-Origin', origin);
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   res.header('Access-Control-Allow-Headers', 'Content-Type');
